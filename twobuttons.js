c2bb72c9ba391f390ac8d8217f6dcfa21247773e
PJ Valentini <pj.valentini@gmail.com>
2017-03-29 15:34:43 -0400

    Add files via upload

---
+++ twobuttons.js
@@ -0,0 +1,10 @@
+//Created the "You are right!" text above button...
+//
+  function showDiv1() {
+    document.getElementById("rightDiv").style.display = "block";
+}
+
+//Created the "You are right!" text above button...
+  function showDiv2() {
+    document.getElementById("wrongDiv").style.display = "block";
+}
