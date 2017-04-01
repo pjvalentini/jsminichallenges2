a385605de58137b41d821ea8a78c0068f9d2e306
PJ Valentini <pj.valentini@gmail.com>
2017-04-01 13:01:48 -0400

    Add files via upload

---
+++ form.js
@@ -0,0 +1,46 @@
+// Given a string, return if the string contains any numbers or not
+// function hasNumbers() {
+//   return !!str.match(/\d+/);
+//
+// var usernameElement = document.getElementById("Username")
+// var value = ducument.getElementById("value")
+
+//Password//
+
+// var passwordElement = document.getElementById("password");
+// console.dir(passwordElement);
+// var form = document.getElementById("form");
+// console.dir(form);
+//
+// form.addEventListener("submit", function(event) {
+// 	console.log(event);
+// 	var correctPassword = passwordElement.value;
+// 	if (correctPassword === "12345678") {
+// 		alert("Password is Correct!");
+// 	}
+// 	else {
+// 		alert("Password is Wrong!");
+//  	}
+// });
+//
+//Username//
+
+var usernameElement = document.getElementById("name");
+console.dir(name);
+
+var form = document.getElementById("form");
+console.log(form);
+
+form.addEventListener("submit", function(event) {
+  console.log(event);
+  var correctUsername = usernameElement.value;
+  if (correctUsername === "!!str.match(/\d+/)") {
+  alert("Username is valid!");
+} else {
+  alert("Password is invalid!");
+  }
+});
+
+// function hasNumbers(name) {
+//   return !!str.match(/\d+/);
+// }
